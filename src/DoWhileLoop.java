964dd7cb948d84f19b141bac4e1245999552afaa
Viola-Ivolga <Viola-Ivolga@users.noreply.github.com>
2022-11-30 20:12:04 -0800

    DoWhile

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,18 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 6;
+
+//        while(i<10)
+//        {
+//            System.out.println(i);
+//            i++;
+//        }
+
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while(i<6);
+
+    }
+}
